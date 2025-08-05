6ac577c60d714700a9aed40e6410ca597448aa66
Zinhle Elizabeth Hlongwane <zinhlehlongwani56@gmail.com>
2025-08-05 16:51:10 +0200

    Update figlet.py

---
+++ Week4/figlet.py
@@ -0,0 +1,20 @@
+import sys
+import random
+from pyfiglet import Figlet
+
+def main():
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+    
+    if len(sys.argv) == 1:
+        font = random.choice(fonts)
+    elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in fonts:
+        font = sys.argv[2]
+    else:
+        sys.exit("Invalid usage")
+    
+    figlet.setFont(font=font)
+    print(figlet.renderText(input("Input: ")))
+
+if __name__ == "__main__":
+    main()
